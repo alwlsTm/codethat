89d078080982f699ee78628485adbaa249317cdd
alwlsTm <mijine0716@naver.com>
2022-06-17 23:19:45 +0900

    20220617 코스 리스트 불러오기

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import { default as data } from './mock.json';
+
+export function getCourses() {  //코스 목록 불러오기
+  return data.courses;
+}
--- src/pages/CourseListPage.js
+++ src/pages/CourseListPage.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { getCourses } from "../api";
 import ListPage from '../components/ListPage';
 import searchBarStyles from '../components/SearchBar.module.css';
 import searchIcon from '../IMGS/search.svg';
 
 function CourseListPage() {
   const [keyword, setKeyword] = useState(''); //검색 키워드 state
+  const courses = getCourses(); //코스 목록 데이터 불러오기
 
   const handleKeywordChange = (e) => setKeyword(e.target.value);
 
@@ -28,6 +30,16 @@ function CourseListPage() {
           <img src={searchIcon} alt="검색"></img>
         </button>
       </form>
+      <p>총 {courses.length}개 코스</p>
+      <div>
+        {courses.map((course) => (
+          <ul>
+            <li>{course.title}</li>
+            <li>{course.summary}</li>
+            <li>언어: {course.language}</li>
+          </ul>
+        ))}
+      </div>
     </>
   );
 }
