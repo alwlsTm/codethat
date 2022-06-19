dbe7e74b22bf4fac9fa11ea4ac0450e854eac4b7
alwlsTm <mijine0716@naver.com>
2022-06-19 18:56:24 +0900

    20220619 Nav - NavLink 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Container from './Container';
 import UserMenu from "./UserMenu";
 import styles from './Nav.module.css';
 import logoImg from '../IMGS/logo.svg';
 
+function getLinkStyle({ isActive }) { //현재 페이지가 내비게이션 링크에 해당될 경우, isActive는 true
+  return {  //인라인 스타일 객체 리턴
+    textDecoration: isActive ? 'underline' : undefined, //isActive가 true일 경우, 텍스트에 밑줄 추가
+  }
+}
+
 function Nav() {
   return (
     <div className={styles.nav}>
       <Container className={styles.container}>
-        <Link to="/"><img src={logoImg} alt="codethat"></img></Link>
+        <Link to="/">
+          <img src={logoImg} alt="codethat"></img>
+        </Link>
         <ul className={styles.menu}>
-          <li><Link to="/courses">카탈로그</Link></li>
-          <li><Link to="/questions">커뮤니티</Link></li>
+          <li>
+            <NavLink to="/courses" style={getLinkStyle}>
+              카탈로그
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/questions" style={getLinkStyle}>
+              커뮤니티
+            </NavLink>
+          </li>
           <li>
             <UserMenu />
           </li>
