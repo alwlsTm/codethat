a64ee711ba028d846f504d34dabd3c4cef0abe2e
alwlsTm <mijine0716@naver.com>
2023-01-19 18:04:10 +0900

    Firebase 설치 & SDK 추가

---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGZohkC3f51OwvaxNeLYwksi4MmnxWxQQ",
+  authDomain: "codethat-5d741.firebaseapp.com",
+  projectId: "codethat-5d741",
+  storageBucket: "codethat-5d741.appspot.com",
+  messagingSenderId: "737899326763",
+  appId: "1:737899326763:web:0f359b7f699b98f5b1296e",
+  measurementId: "G-MLT7RDLG2N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
